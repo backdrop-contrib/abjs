f1a2ea46a2d3b36596440e36f46470a49836667c
Matt Mowers <matt.mowers@gmail.com>
2015-11-05 00:32:23 -0800

    Making recommended pareview updates.

--- abjs-ace-edit.js
+++ abjs-ace-edit.js
@@ -1,19 +1,20 @@
 /**
  * @file
- * Adds ACE code editor to replace and sync with javascript textareas 
+ * Adds ACE code editor to replace and sync with javascript textareas
  */
 
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
+  'use strict';
   if (typeof ace !== 'undefined') {
     jQuery('.form-item-script .form-textarea-wrapper').hide();
     jQuery('.form-item-script .form-textarea-wrapper').after('<div id="editor" style="witdh:100%; height:200px;"></div>');
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
-    editor.getSession().setMode("ace/mode/javascript");    
+    editor.getSession().setMode("ace/mode/javascript");
     editor.getSession().setValue(jQuery('#edit-script').val());
-    editor.getSession().on('change', function(){
+    editor.getSession().on('change', function () {
       jQuery('#edit-script').val(editor.getSession().getValue());
-    });  
+    });
   }
 
 });
