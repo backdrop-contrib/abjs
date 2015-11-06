5d4508b3d2d892cd67888981365424fc69a3b102
Matt Mowers <matt.mowers@gmail.com>
2015-11-05 23:50:22 -0800

    Being explicit that ace is part of window object

--- abjs-ace-edit.js
+++ abjs-ace-edit.js
@@ -5,10 +5,10 @@
 
 jQuery(document).ready(function () {
   'use strict';
-  if (typeof ace !== 'undefined') {
+  if (typeof window.ace !== 'undefined') {
     jQuery('.form-item-script .form-textarea-wrapper').hide();
     jQuery('.form-item-script .form-textarea-wrapper').after('<div id="editor" style="witdh:100%; height:200px;"></div>');
-    var editor = ace.edit("editor");
+    var editor = window.ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/javascript");
     editor.getSession().setValue(jQuery('#edit-script').val());
