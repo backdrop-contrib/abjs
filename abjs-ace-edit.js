1a91afb585955bbc03f135546c4a57b5c5689eac
Matt Mowers <matt.mowers@gmail.com>
2015-11-09 20:31:55 -0800

    Using Drupal behaviors for abjs ace code editor JavaScript

--- abjs-ace-edit.js
+++ abjs-ace-edit.js
@@ -3,18 +3,21 @@
  * Adds ACE code editor to replace and sync with javascript textareas.
  */
 
-jQuery(document).ready(function () {
-  'use strict';
-  if (typeof window.ace !== 'undefined') {
-    jQuery('.form-item-script .form-textarea-wrapper').hide();
-    jQuery('.form-item-script .form-textarea-wrapper').after('<div id="editor" style="witdh:100%; height:200px;"></div>');
-    var editor = window.ace.edit("editor");
-    editor.setTheme("ace/theme/monokai");
-    editor.getSession().setMode("ace/mode/javascript");
-    editor.getSession().setValue(jQuery('#edit-script').val());
-    editor.getSession().on('change', function () {
-      jQuery('#edit-script').val(editor.getSession().getValue());
-    });
-  }
-
-});
+(function ($) {
+  Drupal.behaviors.abjs = {
+    attach: function (context, settings) {
+      'use strict';
+      if (typeof window.ace !== 'undefined') {
+        jQuery('.form-item-script .form-textarea-wrapper').hide();
+        jQuery('.form-item-script .form-textarea-wrapper').after('<div id="editor" style="witdh:100%; height:200px;"></div>');
+        var editor = window.ace.edit("editor");
+        editor.setTheme("ace/theme/monokai");
+        editor.getSession().setMode("ace/mode/javascript");
+        editor.getSession().setValue(jQuery('#edit-script').val());
+        editor.getSession().on('change', function () {
+          jQuery('#edit-script').val(editor.getSession().getValue());
+        });
+      }
+    }
+  };
+})(jQuery);
