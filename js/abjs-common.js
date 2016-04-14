5ebd84f34534e1c5075fa343b081635b3fd5a39d
mmowers <matt.mowers@gmail.com>
2016-04-14 15:16:03 -0700

    fixing ESLint errors and using js strict mode

--- js/abjs-common.js
+++ js/abjs-common.js
@@ -8,20 +8,25 @@
 
 var abCookies = {
   getCookie: function (sKey) {
+    "use strict";
     if (!sKey) {
       return null;
     }
-    abKey = abjs.cookiePrefix + sKey;
+    var abKey = abjs.cookiePrefix + sKey;
     return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(abKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
   },
   setCookie: function (sKey, sValue) {
-    abKey = abjs.cookiePrefix + sKey;
+    "use strict";
+    var abKey = abjs.cookiePrefix + sKey;
     document.cookie = encodeURIComponent(abKey) + "=" + encodeURIComponent(sValue) + "; max-age=" + abjs.cookieLifetime * 24 * 60 * 60 + abjs.cookieDomain + "; path=/" + abjs.cookieSecure;
     return true;
   }
 };
-for (var i = abjs.tests.length - 1; i >= 0; i--) {
-  for (var j = 0; j < abjs.tests[i].conditions.length; j++) {
+
+var i, j;
+
+for (i = abjs.tests.length - 1; i >= 0; i--) {
+  for (j = 0; j < abjs.tests[i].conditions.length; j++) {
     var funcString = abjs.tests[i].conditions[j];
     if (!abjs.tests[i].conditions[j]()) {
       break;
@@ -31,9 +36,9 @@ for (var i = abjs.tests.length - 1; i >= 0; i--) {
     abjs.tests.splice(i, 1);
   }
 }
-for (var i = 0; i < abjs.tests.length; i++) {
+for (i = 0; i < abjs.tests.length; i++) {
   if (abCookies.getCookie(abjs.tests[i].name)) {
-    for (var j = 0; j < abjs.tests[i].experiences.length; j++) {
+    for (j = 0; j < abjs.tests[i].experiences.length; j++) {
       if (abCookies.getCookie(abjs.tests[i].name) === abjs.tests[i].experiences[j].name) {
         abjs.tests[i].activeExperience = j;
         break;
@@ -43,7 +48,7 @@ for (var i = 0; i < abjs.tests.length; i++) {
   else {
     var randomNum = Math.random();
     var fractionSum = 0;
-    for (var j = 0; j < abjs.tests[i].experiences.length; j++) {
+    for (j = 0; j < abjs.tests[i].experiences.length; j++) {
       if (randomNum >= fractionSum && randomNum < fractionSum + abjs.tests[i].experiences[j].fraction) {
         abCookies.setCookie(abjs.tests[i].name,abjs.tests[i].experiences[j].name);
         abjs.tests[i].activeExperience = j;
@@ -53,9 +58,8 @@ for (var i = 0; i < abjs.tests.length; i++) {
     }
   }
 }
-for (var i = 0; i < abjs.tests.length; i++) {
+for (i = 0; i < abjs.tests.length; i++) {
   if (typeof abjs.tests[i].activeExperience !== 'undefined') {
     abjs.tests[i].experiences[abjs.tests[i].activeExperience].script();
   }
 }
-
